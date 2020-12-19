3a39aad6852d67495fd0f94ee8fa1db4a0f2f36d
duthienkt <blueskythien2010@live.com>
2020-12-19 21:55:47 +0700

    - config

--- app/connfig.js
+++ app/connfig.js
@@ -1,2 +1,2 @@
-export var MQTT_SERVER = 'wss://mqtt.absol.cf';
-export var PRIVATE_CHANNEL = 'duthienkt/home';
+export var MQTT_SERVER = 'wss://mqtt.server';
+export var PRIVATE_CHANNEL = 'username/home';
